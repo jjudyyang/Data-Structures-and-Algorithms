5fb5aca20824954df9c87a42c9d594fb604416ac
Judy Yang <judyyang12404@gmail.com>
2022-01-26 21:57:53 -0700

    Time: 8 ms (87.11%), Space: 7.7 MB (80.92%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        //dictionary thingy to keep track of values if any repeating
+        vector<int> characters(128);
+        
+        int left = 0;
+        int right = 0;
+        
+        int result = 0;
+        
+        while(right < s.length()){ 
+            //while right slider has not reached end of string
+            char letterRight = s[right];
+            characters[letterRight]++;
+            
+            //if the character count for the letter is greater than 1 means repeating letter
+            while(characters[letterRight] > 1){
+                char letterLeft = s[left];
+                characters [letterLeft]--;
+                left++;
+            }
+            
+            if(result < right - left + 1 ){
+                result = right - left + 1;
+            }
+            else{
+                
+            }
+            right++;
+            
+        }
+        return result;
+    }
+};
